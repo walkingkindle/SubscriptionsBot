85d9a11c179109c01385f36ebb54a2205f2ada78
aleksahadzic22 <aki.hadza@gmail.com>
2025-10-26 21:34:00 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,37 @@
+using Application.Services;
+using Domain.Interface;
+using Infrastructure;
+
+namespace SubscriptionsBot;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // This is where you register services like DB, authentication, etc.
+        services.AddControllers();
+
+        services.AddDbContext<ApplicationDbContext>();
+
+        services.AddTransient<ISubscriberService, SubscriberService>();
+
+        services.AddTransient<ISubscriptionService, SubscriptionService>();
+
+        services.AddTransient<IPaymentService, PaymentService>();
+
+        services.AddTransient<IOutstandingPaymentsService, OutstandingPaymentsService>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
